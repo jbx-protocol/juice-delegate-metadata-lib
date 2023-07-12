b59db7869d6eec1ac3e74624797dc7d83dbed320
DrGorilla_md <dr_gorilla@protonmail.com>
2023-07-12 23:04:50 +0200

    wip

--- test/JBXDelegateMetadataLib.sol
+++ test/JBXDelegateMetadataLib.sol
@@ -19,15 +19,16 @@ contract JBXDelegateMetadataLib_Test is Test {
             bytes32(uint256(type(uint256).max)),    // First 32B reserved
             
             bytes4(0x11111111), // First delegate id == 2
-            bytes1(uint8(110)),       // First delegate offset:
+            bytes1(uint8(132)),       // First delegate offset:
             // 4B function selector
             // 32B _id arg
             // 32B pointer to the bytes
             // 32B bytes length
-            // Then start the actual metadata -> offset are shifted by 100 -> first is 110
+            // 32B reserved
+            // Then start the actual metadata -> offset are shifted by 132
 
             _id,                     // Second delegate id == _id
-            bytes1(uint8(110+32)),       // Second delegate offset (first offset + length of first metadata)
+            bytes1(uint8(132+32)),       // Second delegate offset (first offset + length of first metadata)
 
             bytes32(0),   // First delegate metadata
             bytes32(uint256(type(uint256).max))   // Second delegate metadata
@@ -57,7 +58,7 @@ contract ForTest_JBXDelegateMetadataLib {
             let _current := 132 // function  selector 4B + _id one word + bytes pointer (0x40) 1 word + bytes length 1 word + reserved for protocol 1 word
 
             // End of the id/offset: take the first offset
-            let _end := add(132, shr(248, calldataload(add(_current, 4))))
+            let _end := shr(248, calldataload(add(_current, 4)))
 
             // Iterate over every ID:
             for {  } lt(_current, _end) { _current := add(_current, 5) }
@@ -69,25 +70,26 @@ contract ForTest_JBXDelegateMetadataLib {
 
                 // If we found the ID, copy the metadata
                 if eq(_currentId, _id) {
-                    _targetMetadata := _id
+                        _targetMetadata := _end
+
                     // If it's the last one, copy until the end
                     switch eq(add(_current, 5), _end)
                     case true {
-                        let _dataSize := sub(calldatasize(), _currentOffset)
+                        // let _dataSize := sub(mul(calldatasize(), 8), add(_currentOffset, 132))
 
-                        mstore(0x0, _dataSize)
+                        // mstore(0x0, _dataSize)
 
-                        calldatacopy(0x20, _currentOffset, _dataSize)
+                        // calldatacopy(0x20, add(_currentOffset, 132), _dataSize)
 
-                        return(0x0, add(_dataSize, 32))
+                        // return(0x0, add(_dataSize, 32))
                     }
                     // Otherwise, copy until the next offset
-                    default {
-                        let _nextOffset := and(shr(32, calldataload(add(_current, 5))), 0xFF)
-                        _targetMetadata := mload(0x40)
-                        mstore(0x40, add(_targetMetadata, sub(_nextOffset, add(_currentOffset, 1))))
-                        calldatacopy(_targetMetadata, add(_currentOffset, 1), sub(_nextOffset, add(_currentOffset, 1)))
-                    }
+                    // default {
+                    //     let _nextOffset := and(shr(32, calldataload(add(_current, 5))), 0xFF)
+                    //     _targetMetadata := mload(0x40)
+                    //     mstore(0x40, add(_targetMetadata, sub(_nextOffset, add(_currentOffset, 1))))
+                    //     calldatacopy(_targetMetadata, add(_currentOffset, 1), sub(_nextOffset, add(_currentOffset, 1)))
+                    // }
                 }                
             }
 
