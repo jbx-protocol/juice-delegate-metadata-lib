76068155fef1e9551952ba1e0db331b3f6b35841
DrGorilla_md <dr_gorilla@protonmail.com>
2023-07-16 23:05:51 +0200

    fix: this works

--- test/JBXDelegateMetadataLib.sol
+++ test/JBXDelegateMetadataLib.sol
@@ -21,13 +21,14 @@ contract JBXDelegateMetadataLib_Test is Test {
             
             // -- offset 1 --
             bytes4(0x11111111), // First delegate id
-            bytes1(uint8(2)),       // First delegate offset == 2
+            uint8(2),       // First delegate offset == 2
             _id,                     // Second delegate id == _id
-            bytes1(uint8(3)),       // Second delegate offset
+            uint8(3),       // Second delegate offset == 3
+            bytes22(0),     // Rest of the word is 0-padded
 
             // -- offset 2 --
-            bytes32(0),   // First delegate metadata
-            
+            bytes32(hex'deadbeefdeadbeef'),   // First delegate metadata
+
             // -- offset 3 --
             bytes32(uint256(type(uint256).max))   // Second delegate metadata
         );
@@ -42,29 +43,37 @@ contract JBXDelegateMetadataLib_Test is Test {
  * @dev Harness to deploy and test JBXDelegateMetadataLib
  */
 contract ForTest_JBXDelegateMetadataLib {
-    function getMetadata(bytes4 _delegateId, bytes calldata _metadata) external pure returns(bytes memory _targetMetadata) {
+    event TestStuff(uint);
+    event TestB(bytes32);
+
+    function getMetadata(bytes4 _delegateId, bytes calldata _metadata) external returns(bytes memory _targetMetadata) {
         // Either no metadata or empty one with only one selector (32+4+1)
-        if(_metadata.length < 37) revert(); //return bytes1(0);
+        if(_metadata.length < 37) revert(); //return bytes(0);
 
         // Parse the id's -> stop when next offset == 0 or current = first offset
-        uint256 _firstOffset;
+        uint8 _firstOffset = uint8(_metadata[32+4]);
 
-        for(uint256 _i; _metadata[_i + 4] != bytes1(0) && uint256(bytes32(_metadata[_i + 4])) != _firstOffset; _i += 4) {
-            _firstOffset = uint256(bytes32(_metadata[_i + 4]));
+        for(uint256 _i = 32; _metadata[_i] != bytes1(0) && uint8(_metadata[_i]) != _firstOffset * 32; _i += 5) {
+            emit TestStuff(_i);
+            emit TestB(bytes4(_metadata[_i:_i+4]));
+            emit TestStuff(uint8(_metadata[_i + 4]));
+
 
             // id found?
-            if(bytes4(_metadata[_i]) == _delegateId) {
+            if(bytes4(_metadata[_i:_i+4]) == _delegateId) {
+            emit TestStuff(69696969);
+
                 // End of the calldata (either start of data's or next offset is 0)
-                if(_i + 5 > _firstOffset || _metadata[_i + 9] == 0) {
-                    return _metadata[uint256(bytes32(_metadata[_i + 4])) : _metadata.length];
+                if(_i + 5 == _firstOffset || _metadata[_i + 9] == 0) {
+                    emit TestStuff(uint8(_metadata[_i + 4]));
+
+                    return _metadata[uint8(_metadata[_i + 4]) * 32 : _metadata.length];
                 }
 
                 // If not, only return until next offset
-                return _metadata[uint256(bytes32(_metadata[_i + 4])) : uint256(bytes32(_metadata[_i + 9]))];
+                return _metadata[uint8(_metadata[_i + 4]) * 32 : uint8(_metadata[_i + 9]) * 32];
             }
         }
-        
-
     }
 
     /**
