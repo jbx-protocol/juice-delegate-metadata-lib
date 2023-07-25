ef35f4f6d7267fd5b26476f9518dc9b133d8ff85
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2023-07-25 23:39:27 +0200

    style: changed order of constants

--- src/JBXDelegateMetadataLib.sol
+++ src/JBXDelegateMetadataLib.sol
@@ -31,11 +31,11 @@ library JBXDelegateMetadataLib {
     uint256 constant ID_OFFSET_SIZE = 1;
     uint256 constant WORD_SIZE = 32;
 
-    // The amount of bytes to go forward to get to the offset of the next delegate (aka. the end of the offset of the current delegate).
-    uint256 constant NEXT_DELEGATE_OFFSET = TOTAL_ID_SIZE + ID_SIZE;
-    
     // The size that a delegate takes in the lookup table (Identifier + Offset).
     uint256 constant TOTAL_ID_SIZE = ID_SIZE + ID_OFFSET_SIZE;
+
+    // The amount of bytes to go forward to get to the offset of the next delegate (aka. the end of the offset of the current delegate).
+    uint256 constant NEXT_DELEGATE_OFFSET = TOTAL_ID_SIZE + ID_SIZE;
 
     // 1 word (32B) is reserved for the protocol .
     uint256 constant RESERVED_SIZE = 1 * WORD_SIZE;
